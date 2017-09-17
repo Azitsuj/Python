cf49879071c228b6bb54a6a6d0dd64149bfdcafa
Konrad <k.jaroslawski@gmail.com>
2017-09-17 11:58:07 +0200

    Python podstawy cz.1a

--- Podstawy/podstawy.py
+++ Podstawy/podstawy.py
@@ -1,3 +1,5 @@
+# -*- coding: utf-8 -*-
+
 # Zmienne napisowe
 zmienna1 = 'abc'
 zmienna2 = "Konrad"
@@ -70,11 +72,71 @@ print(imie + '\t' + nazwisko + ';' + rokUrodzenia + ';' + stanowisko + ';' + str
 
 # P6 Pole powierzchni kola
 pi = 3.14
-# r = 5
-r = int(input('Podaj promien kola '))
+r = 5
+# r = int(input('Podaj promien kola '))
 polePowierzchniKola = pi * (r ** 2)
 print('Pole kola o promieniu ' + str(r) + ' = ' + str(polePowierzchniKola))
 
 # P7 Proba przypisania zmiennej do slowa kluczowego
 """and = 3
 print(and)"""
+
+a = None
+print(a)
+print(type(a))
+
+liczbaZesp1 = 2 + 3j
+liczbaZesp2 = 1 + 1j
+print(liczbaZesp1 + liczbaZesp2)
+print(type(liczbaZesp1))
+
+a = 3.4e+5
+print(a)
+
+# zrzucenie dziesietnej czesci z dzielenia
+a = 3.0 // 2.0
+print(a)
+
+# P10 cena brutto netto
+imie = 'Adam'
+nazwisko = 'Kowalski'
+rokUrodzenia = '1950-06-15'
+stanowisko = 'dyrektor'
+placaB = 5050.50
+placaN = round(placaB/1.23,2)
+print(imie + '\t' + nazwisko + ';' + rokUrodzenia + ';' + stanowisko + ';' + str(placaN) + ';' + str(placaB))
+
+# P11
+chleb = 1.99
+mleko = 2.50
+cukierki = 12.99
+# zamowienie to 2 szt. chleba, 0.5 l mleka, 300 g cukierkow
+zamowienie = round(2 * chleb + 0.5 * mleko + 0.3 * cukierki, 2)
+print('zamowienie kosztuje ' + str(zamowienie) + ' zl')
+
+a = 12
+b = 1 -1j
+# b = 1 + (-1)j - bledna definicja liczby zespolonej, j musi byc bezposrednio przy liczbie
+print(a*b)
+
+""" wejscie = input('Podaj napis: ')
+flaga = bool(wejscie)
+print(flaga) """
+quote = '"ecie pecie"'
+quote2 = "'pecie ecie'"
+print(quote)
+print(quote2)
+print('Ten napis'+'\n'+'ma'+'\n'+'wiele'+'\n'+'linii')
+print('Ten napis \tma \twiele \ttabulatorow')
+print('''abc
+cda
+koniec''')
+# zmienia typ zakonczenia printa, zamiast nowej linii jako domyslnej, ustawia '...'
+print('abc'+'\n'+'csa', end='...')
+print('kolejny napis')
+print('abc'+'\n'+'csa', end='')
+print('kolejny napis')
+koniec = 'abc'
+print('abc'+'\n'+'csa', end=koniec)
+print('kolejny napis')
+
